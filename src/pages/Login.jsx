a38681e01c46897ab90b4c42665bcb2bfa34e21f
escape-engineering <tkdrnr1215@naver.com>
2024-09-09 22:31:49 +0900

    style: 로그인페이지 style 수정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -24,29 +24,33 @@ const Login = () => {
                 navigate("/");
             }
         } catch (error) {
-            console.log("error :>> ", error);
-            // alert(error.response.data.message);
+            if (error.status === 401) alert("아이디나 비밀번호가 올바르지 않습니다!");
         }
     };
 
     return (
-        <form className="flex flex-col justify-center" onSubmit={handleOnSubmit}>
+        <form className="flex flex-col justify-center items-center gap-5" onSubmit={handleOnSubmit}>
             <input
-                className="border-solid border-black border-2 w-64"
+                className="border-solid border-gray-300 border-2 w-96 h-16 rounded-3xl indent-5"
                 type="text"
                 placeholder="아이디를 입력해주세요"
                 value={userId}
                 onChange={handleUserId}
             />
-            <p className="border-solid border-black border-2 w-64 h-7"></p>
             <input
-                className="border-solid border-black border-2 w-64"
+                className="border-solid border-gray-300 border-2 w-96 h-16 rounded-3xl indent-5"
                 type="password"
                 placeholder="비밀번호를 입력해주세요"
                 value={userPassword}
                 onChange={handleUserPassword}
             />
-            <button type="form" disabled={!isReadyToLogin} className="border-solid border-black border-2 w-40 h-7">
+            <button
+                type="form"
+                disabled={!isReadyToLogin}
+                className={`border-${
+                    isReadyToLogin ? "solid" : "dashed"
+                } border-gray-400 border-2 rounded-xl w-40 h-7 ${isReadyToLogin ? "hover:bg-gray-300" : ""}`}
+            >
                 로그인
             </button>
         </form>

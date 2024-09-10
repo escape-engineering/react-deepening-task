759e65dae757a20672aa824ab5f89e47030a8567
escape-engineering <tkdrnr1215@naver.com>
2024-09-10 11:46:33 +0900

    style: profile페이지 스타일 수정

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -62,26 +62,66 @@ const Profile = () => {
     return (
         <>
             {!isUpdating ? (
-                <div>
-                    <img src={avatar ? avatar : defaultProfile} alt="유저 프로필 이미지" />
+                <div className="flex flex-col justify-center items-center gap-3">
+                    <img
+                        className="w-[300px] h-[300px] object-fill"
+                        src={avatar ? avatar : defaultProfile}
+                        alt="유저 프로필 이미지"
+                    />
                     <p>닉네임: {nickname}</p>
                     <p>ID: {userId}</p>
-                    <button onClick={toggleIsUpdating}>수정하기</button>
+                    <button
+                        className="border-solid border-[#dfdfdf] border-4 rounded-xl hover:bg-slate-200 p-1"
+                        onClick={toggleIsUpdating}
+                    >
+                        수정하기
+                    </button>
                 </div>
             ) : (
-                <form onSubmit={(e) => updateUserInfo(e)}>
-                    {/* Todo - 드래그드롭으로 이미지 직접넣기 */}
-                    <img src={updatedImgFile ? URL.createObjectURL(updatedImgFile) : avatar} />
-                    <label>이미지주소</label>
-                    <input type="file" accept="image/jpeg,image/jpg,image/png" onChange={handleUpdatedImgFile} />
-                    <label>닉네임</label>
-                    <input type="text" placeholder={`이전 닉네임: ${nickname}`} onChange={handleupdatedNickname} />
+                <form className="flex flex-col justify-center items-center gap-3" onSubmit={(e) => updateUserInfo(e)}>
+                    {/* //TODO - 드래그드롭으로 이미지 직접넣기 */}
+                    <img
+                        className="w-[300px] h-[300px] object-fill"
+                        src={updatedImgFile ? URL.createObjectURL(updatedImgFile) : avatar}
+                    />
+                    <label
+                        htmlFor="userProfileInput"
+                        className="px-2 w-[180px] h-[100px] border-solid border-[#dfdfdf] border-4 rounded-xl flex text-center items-center"
+                    >
+                        클릭, 파일 드롭으로 추가해주세요!
+                    </label>
+                    <input
+                        type="file"
+                        id="userProfileInput"
+                        accept="image/jpeg,image/jpg,image/png"
+                        onChange={handleUpdatedImgFile}
+                        className="w-0 h-0"
+                    />
+                    <div className="flex flex-row gap-2 justify-center items-center">
+                        <label>닉네임</label>
+                        <input
+                            type="text"
+                            value={updatedNickname}
+                            placeholder={`${nickname}`}
+                            onChange={handleupdatedNickname}
+                            className="h-[30px] border-solid border-[#dfdfdf] border-4 rounded-xl indent-2 py-4"
+                        />
+                    </div>
                     <p>ID: {userId}</p>
-                    <div>
-                        <button type="button" onClick={(e) => toggleIsUpdating(e)}>
+                    <div className="flex flex-row justify-center items-center gap-4">
+                        <button
+                            type="button"
+                            onClick={(e) => toggleIsUpdating(e)}
+                            className="border-solid border-[#dfdfdf] border-4 rounded-xl hover:bg-slate-200 p-1"
+                        >
                             취소
                         </button>
-                        <button type="form">수정완료</button>
+                        <button
+                            type="form"
+                            className="border-solid border-[#dfdfdf] border-4 rounded-xl hover:bg-slate-200 p-1"
+                        >
+                            수정완료
+                        </button>
                     </div>
                 </form>
             )}
